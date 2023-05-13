d6a609a291c16025ae3cf9c01e5624e5df5c11b9
eggonz <egoitz.gonz@gmail.com>
2023-05-13 16:07:03 +0200

    fix eval.sh

--- scripts/eval.sh
+++ scripts/eval.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-EXP_NAME=$1  # input argumennt
+EXP_PATH=$(pwd)/$1  # input argument
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 ROOT=$DIR/..
 
 cd $ROOT/src
 
-accelerate launch --config_file accelerate_config.yaml eval.py --batch-size 32 --data-path $ROOT/data --results-path $ROOT/results/$EXP_NAME/
+accelerate launch --config_file accelerate_config.yaml eval.py --batch-size 32 --data-path $ROOT/data --results-path $EXP_PATH/
